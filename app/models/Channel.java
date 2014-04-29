35fead43dd3753be82cc740081eef4f882278906
Boldinova Natalya <boldinova_natalya@mail.ru>
2014-04-29 15:35:06 +0400

    Added addChannel in Channel class

--- app/models/Channel.java
+++ app/models/Channel.java
@@ -1,12 +1,19 @@
 package models;
 
 
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 import play.data.validation.Required;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 import java.util.List;
 
@@ -16,7 +23,7 @@ public class Channel extends Model {
     public List<User> users;
 
     @OneToMany
-    public List<Channel> channels;
+    public List<Item> items;
 
     @Required
     public String title;
@@ -39,16 +46,25 @@ public class Channel extends Model {
         url = "url";
         pubDate = new Date();
         image = "jfksjkf";
-        create();
+        //create();
     }
 
+    public Channel(String title, String description, String url, Date pubDate, String image) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.pubDate = pubDate;
+        this.image = image;
+      //  create();
+    }
 
     public static List<Channel> getChannels(User user, int page, int length) {
 
-        return find("user", user).fetch(page, length);
+        //return find("user", user).fetch(page, length);
+        return findAll();
     }
 
-    public static Channel getChannel(int channelId) {
+    public static Channel getChannel(long channelId) {
         return find("id", channelId ).first();
     }
 
@@ -70,6 +86,28 @@ public class Channel extends Model {
         refresh();
         user.refresh();
 
+    }
+
+    public static void addChannel(String urlString) throws IOException, FeedException {
+
+        URL url = new URL(urlString);
+        HttpURLConnection httpcon = (HttpURLConnection)url.openConnection();
+        // Reading the feed
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed channel = input.build(new XmlReader(httpcon));
+        String img = channel.getImage() != null ? channel.getImage().getUrl() : "";
+        Channel ch = new Channel(channel.getTitle(), channel.getDescription(),
+                channel.getUri(),
+                channel.getPublishedDate(),
+                img);
+        if (ch.create()) {
+            play.Logger.info("channel created");
+            play.Logger.info("%d", ch.id);
+            play.Logger.info("%s", ch.description );
+            play.Logger.info("%s", channel.getDescription());
+        } else {
+            play.Logger.info("channel not created");
+        }
     }
 
 
